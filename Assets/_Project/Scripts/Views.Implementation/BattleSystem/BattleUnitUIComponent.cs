16b011a4f84b66e01b028017fe7be366f1cc4df0
Alexander Kotov <newbeedevinc@gmail.com>
2026-02-05 18:33:54 +0700

    visual improvements

--- Assets/_Project/Scripts/Views.Implementation/BattleSystem/BattleUnitUIComponent.cs
+++ Assets/_Project/Scripts/Views.Implementation/BattleSystem/BattleUnitUIComponent.cs
@@ -11,11 +11,32 @@ namespace CityBuilder.Views.Implementation.BattleSystem
         public Slider healthSlider;
         public TextMeshProUGUI healthText;
         
+        public Vector2 MaxHealthBarSize = new Vector2(100f, 10f);
+        public Vector2 MinHealthBarSize = new Vector2(30f, 10f);
+        
         private IBattleUnit _battleUnit;
 
         public void Init(IBattleUnit battleUnit)
         {
             _battleUnit = battleUnit;
+            UpdateSize();
+        }
+
+        private void UpdateSize()
+        {
+            const float maxSize = 1000f;
+            
+            var startHealth = _battleUnit.Health.StartValue.Value;
+            var rectTransform = (RectTransform)healthSlider.transform;
+            
+            if (startHealth > maxSize)
+            {
+                rectTransform.sizeDelta = MaxHealthBarSize;
+            }
+            else
+            {
+                rectTransform.sizeDelta = Vector2.Lerp(MinHealthBarSize, MaxHealthBarSize, startHealth / maxSize);
+            }
         }
 
         private void Update()
